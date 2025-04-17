3009119507842a0b5ecc9e2280e77686d2cc12df
zhedian <cooldeer@126.com>
2025-04-17 09:40:59 +0800

    Fix oxidizer

--- core/flame_temperature.py
+++ core/flame_temperature.py
@@ -5,7 +5,7 @@ def estimate_flame_temperature(fuel, oxidizer, phi):
     gas.set_equivalence_ratio(phi, fuel=fuel, oxidizer=oxidizer)
     gas.TP = 300, ct.one_atm
 
-    gas.set_equivalence_ratio(phi, fuel=fuel, oxidizer="O2:1, N2:3.76")
+    gas.set_equivalence_ratio(phi, fuel=fuel, oxidizer=oxidizer)
     gas.equilibrate("HP")  # 等焓等压平衡燃烧
     flame_temp = gas.T
 
